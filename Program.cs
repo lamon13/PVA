77c9a69fe9a495c254fcde7a61f08a5ad205912f
Kůrka Ondřej <kurka.on.2022@skola.ssps.cz>
2022-11-09 08:34:00 +0100

    L

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace opakovani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int cislo = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < cislo; i++)
+                {
+                    Console.WriteLine("Cyklus: " + (i + 1));
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
